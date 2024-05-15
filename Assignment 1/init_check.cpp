21b0197d5a7cb3e2f47f7f9bf22005d08f2be24b
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-05-15 09:24:56 +0530

    Update init_check

--- Assignment 1/init_check.cpp
+++ Assignment 1/init_check.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
 #include<fstream>
 #include<stdlib.h>
+#include<string>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     if (argc != 2) {
-        cout << "Program usage: init <filename>";
+        cout << "Program usage: init help | init <filename>";
         return -1;
     }
 
-    string filename = argv[1];
+    string helpText = "help";
+
+    if (helpText.compare(argv[1]) == 0) {
+        cout << "TODO FORMATED README";
+        return 0;
+    }
+
+    string fileName = argv[1];
+    cout << fileName;
 
     return 0;
 }
