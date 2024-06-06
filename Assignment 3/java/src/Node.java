2dd6f999cefce1625df9284d5f4905d0af86c746
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-07 00:02:23 +0530

    Add node details

--- Assignment 3/java/src/Node.java
+++ Assignment 3/java/src/Node.java
@@ -1,13 +1,38 @@
-public class Node {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class Node extends JButton implements Runnable {
  
     public String id;
     public boolean isPHold;
+    public Token token;
+
     private Node nextNode;
+
+    public boolean isRequestingCS = false;
+    public boolean isInCS = false;
+
+    private boolean isRequestSent = false;
+
+    private ActionListener actionListener = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            if (!isRequestingCS && !isInCS) {
+                isRequestingCS = true;
+            }
+        };
+    };
  
     public Node(String id) {
+        super(id);
+
         this.id = id;
         this.isPHold = false;
+        this.token = null;
         this.nextNode = null;
+
+        this.addActionListener(actionListener);
     }
  
     public void setNextNode(Node node) {
@@ -15,5 +40,32 @@ public class Node {
     }
     public Node getNextNode() {
         return this.nextNode;
+    }
+
+    public void sendRequest(Node node) {
+
+        if (this.isPHold) {
+            if (!this.id.equals(node.id)){
+                token.queue.add(node);
+            }
+        }
+        else {
+            if (this.nextNode != null) {
+                this.nextNode.sendRequest(node);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        if (isRequestingCS) {
+            if (isPHold) {
+
+            }
+            else if (!isRequestSent) {
+                this.nextNode.sendRequest(this);
+                isRequestSent = true;
+            }
+        }
     }
 }
--- Assignment 3/java/src/Token.java
+++ Assignment 3/java/src/Token.java
@@ -3,5 +3,16 @@ import java.util.Queue;
  
 public class Token {
     public Node node;
-    public Queue<Node> queue = new ArrayDeque<>();
+    public Queue<Node> queue;
+
+    public Token() {
+        this.node = null;
+        this.queue = new ArrayDeque<>();
+    }
+
+    public Token setNode(Node node) {
+        this.node = node;
+        
+        return this;
+    }
 }
