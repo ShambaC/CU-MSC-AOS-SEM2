361e61b682cc25f32d7a9c3c2e1ced381031766e
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-07 18:08:17 +0530

    Update Main

--- Assignment 3/java/src/Main.java
+++ Assignment 3/java/src/Main.java
@@ -1,7 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class Main extends JFrame {
 
@@ -24,5 +31,32 @@ public class Main extends JFrame {
         token.setLocation(randomStarterNode.getLocation().x, randomStarterNode.getLocation().y + randomStarterNode.getHeight());
         randomStarterNode.token = token;
         randomStarterNode.isPHold = true;
+
+        // Start threads
+
+
+        // Log token details
+        Timer timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(token);
+            }
+        });
+
+        timer.setRepeats(true);
+        if (logTokenDetails) {
+            timer.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Log the output to a file
+        try {
+            PrintStream fileStream = new PrintStream("outputLog.txt");
+            System.setOut(fileStream);
+        }
+        catch (FileNotFoundException err) {
+            err.printStackTrace();
+        }
     }
 }
