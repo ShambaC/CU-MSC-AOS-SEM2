f571354cdda11c6f4f282f6b1f45204dd388e9cf
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-07 17:54:30 +0530

    Add main method for token ring

--- Assignment 3/java/src/Main.java
+++ Assignment 3/java/src/Main.java
@@ -1,3 +1,28 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+    static List<Node> nodeList = new ArrayList<>();
+
+    private int nodes;
+    private boolean logTokenDetails;
     
+    public Main() {
+        init();
+    }
+
+    private void init() {
+
+
+
+        int index = (int) Math.random() * nodeList.size();
+        Node randomStarterNode = nodeList.get(index);
+        Token token = new Token().setNode(randomStarterNode);
+        token.setLocation(randomStarterNode.getLocation().x, randomStarterNode.getLocation().y + randomStarterNode.getHeight());
+        randomStarterNode.token = token;
+        randomStarterNode.isPHold = true;
+    }
 }
