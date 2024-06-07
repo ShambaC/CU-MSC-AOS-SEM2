0e103ab9eec7c3ff2771e01653926bd0ca5ab68b
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-07 17:26:06 +0530

    Complete token ring logic

--- Assignment 3/java/src/Node.java
+++ Assignment 3/java/src/Node.java
@@ -1,7 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
+import javax.swing.Timer;
 
 public class Node extends JButton implements Runnable {
  
@@ -19,6 +21,7 @@ public class Node extends JButton implements Runnable {
     private ActionListener actionListener = new ActionListener() {
         public void actionPerformed(ActionEvent e) {
             if (!isRequestingCS && !isInCS) {
+                System.out.println("\nNode " + id + " is requesting for CS");
                 isRequestingCS = true;
             }
         };
@@ -43,16 +46,87 @@ public class Node extends JButton implements Runnable {
     }
 
     public void sendRequest(Node node) {
+        System.out.println("\nNode " + node.id + " has sent a request to Node " + id);
 
         if (this.isPHold) {
-            if (!this.id.equals(node.id)){
-                token.queue.add(node);
+            if (isInCS) {
+                if (!this.id.equals(node.id)){
+                    System.out.println("\nNode " + id + " is in CS, adding the request of Node " + node.id + " to the queue");
+                    token.queue.add(node);
+                }
+            }
+            else {
+                System.out.println("\nNode " + id + " is p_hold but not in CS, forwarding the token to next Node " + this.nextNode.id + ", destination: Node " + node.id);
+                token.setNode(node);
+                this.isPHold = false;
+                this.nextNode.sendToken(token);
             }
         }
         else {
             if (this.nextNode != null) {
+                System.out.println("\nNode " + id + " is not the p_hold, forwarding the request to Node " + this.nextNode.id);
                 this.nextNode.sendRequest(node);
             }
+        }
+    }
+
+    public void sendToken(Token recievedToken) {
+        if (recievedToken.node.id.equals(this.id)) {
+            System.out.println("\nRecieved token at Node " + id + ", this is the required destination. Making it p_hold");
+            this.isPHold = true;
+            this.token = recievedToken;
+            this.token.setIsAtLocation(true);
+            this.token.setLocation(this.getLocation().x, this.getLocation().y + this.getHeight());
+        }
+        else {
+            System.out.println("\nRecieved token at Node " + id + ", not the required destination, forwarding token to Node " + this.nextNode.id);
+            recievedToken.setIsAtLocation(false);
+            nextNode.sendToken(recievedToken);
+        }
+    }
+
+    private void startCS() {
+        Random random = new Random(System.currentTimeMillis());
+        int randomDelay = random.nextInt(1, 11);
+
+        System.out.println("\nNode " + id + " going into CS for " + randomDelay + "s\n");
+
+        Timer countDownTimer = new Timer(1000, new ActionListener() {
+            int remainingTime = randomDelay;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                remainingTime--;
+
+                setText(id + ", " + remainingTime);
+            }
+        });
+
+        Timer timer = new Timer(randomDelay * 1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                countDownTimer.stop();
+                setText(id);
+                stopCS();
+            }
+        });
+
+        countDownTimer.setRepeats(true);
+        timer.setRepeats(false);
+
+        countDownTimer.start();
+        timer.start();
+    }
+
+    private void stopCS() {
+        isInCS = false;
+
+        if (!token.queue.isEmpty()) {
+            Node nextInQueue = token.queue.poll();
+
+            token.setNode(nextInQueue);
+            isPHold = false;
+            this.nextNode.sendToken(token);
         }
     }
 
@@ -62,7 +136,9 @@ public class Node extends JButton implements Runnable {
             while (true) {
                 if (isRequestingCS) {
                     if (isPHold) {
-        
+                        isRequestingCS = false;
+                        isInCS = true;
+                        startCS();
                     }
                     else if (!isRequestSent) {
                         this.nextNode.sendRequest(this);
--- Assignment 3/java/src/Token.java
+++ Assignment 3/java/src/Token.java
@@ -1,9 +1,15 @@
+import java.awt.Graphics;
+import java.awt.Point;
 import java.util.ArrayDeque;
 import java.util.Queue;
+
+import javax.swing.JPanel;
  
-public class Token {
+public class Token extends JPanel {
     public Node node;
     public Queue<Node> queue;
+
+    private boolean isAtLocation = false;
 
     public Token() {
         this.node = null;
@@ -14,5 +20,19 @@ public class Token {
         this.node = node;
         
         return this;
+    }
+
+    public boolean isAtLocation() {
+        return this.isAtLocation;
+    }
+    public void setIsAtLocation(boolean value) {
+        this.isAtLocation = value;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Point coords = getLocation();
     }
 }
