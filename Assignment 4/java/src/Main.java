08e52b8295e5629f135a602f083c51152e9bfb55
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-17 02:31:22 +0530

    add UI to raymond tree

--- Assignment 4/java/src/Main.java
+++ Assignment 4/java/src/Main.java
@@ -1,13 +1,85 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Point;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.io.IOException;
 import java.nio.file.Files;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.border.Border;
+
+/**
+ * Utility class to create a round button
+ */
+class RoundedBorder implements Border{
+
+	private int radius;
+
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius, this.radius, this.radius, this.radius);
+    }
+
+    public boolean isBorderOpaque() {
+        return false;
+    }
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width, height, radius*5, radius*5);
+    }
+}
+
+class Container extends JPanel {
+    private List<Node> nodeList;
+
+    public Container() {
+        super();
+    }
+
+    public void setList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node node = nodeList.get(i);
+
+            Point p1 = node.getLocation();
+            if (node.parent != null) {
+                Point p2 = node.parent.getLocation();
+
+                g.drawLine(p1.x, p1.y, p2.x, p2.y);
+                g.fillOval(p2.x, p2.y, 5, 5);
+            }
+        }
+    }
+}
 
 public class Main extends JFrame {
     private List<Node> nodeList = new ArrayList<>();
+    private Container container = new Container();
 
     public Main() {
         setTitle("Raymond algorithm");
@@ -17,6 +89,14 @@ public class Main extends JFrame {
     }
 
     private void init() {
+        // Define the colors
+        Color buttonBgIdle = new Color(185, 235, 255);
+        Color buttonBgInCS = new Color(166, 88, 76);
+        Color buttonBgRqst = new Color(201, 192, 133);
+        Color buttonFg = new Color(0, 17, 61);
+
+        container.setLayout(null);
+
         JFileChooser fc = new JFileChooser();
         int res = fc.showOpenDialog(this);
 
@@ -25,10 +105,127 @@ public class Main extends JFrame {
                 String content = new String(Files.readAllBytes(fc.getSelectedFile().toPath()));
 
                 // TODO LOGIC
+
+                // Test
+                String[] lines = content.split("\\r\\n");
+                int size = Integer.parseInt(lines[0].strip());
+
+                for (int i = 1; i < lines.length; i++) {
+                    Node node1 = new Node(lines[i].split(" ")[0], size);
+                    Node node2 = new Node(lines[i].split(" ")[1], size);
+
+                    node1.setSize(50, 50);
+                    node1.setBorder(new RoundedBorder(20));
+                    node1.setBackground(buttonBgIdle);
+                    node1.setForeground(buttonFg);
+
+                    node2.setSize(50, 50);
+                    node2.setBorder(new RoundedBorder(20));
+                    node2.setBackground(buttonBgIdle);
+                    node2.setForeground(buttonFg);
+
+                    if (!nodeList.contains(node1)) {
+                        nodeList.add(node1);
+                        container.add(node1);
+                    }
+                    else {
+                        node1 = nodeList.get(nodeList.indexOf(node1));
+                    }
+                    if (!nodeList.contains(node2)) {
+                        nodeList.add(node2);
+                        container.add(node2);
+                    }
+                    else {
+                        node2 = nodeList.get(nodeList.indexOf(node2));
+                    }
+
+                    node1.parent = node2;
+                }
+
+                render();
+                add(container, BorderLayout.CENTER);
             }
             catch (IOException err) {
                 err.printStackTrace();
             }
         }
+    }
+
+    /**
+     * Method to render the tree
+     */
+    private void render() {
+        Map<Node, Integer> nodeLevels = new HashMap<>();
+
+        // Assign node levels
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node node = nodeList.get(i);
+            int level = calcLevel(node);
+
+            nodeLevels.put(node, level);
+        }
+
+        // Draw the tree
+        int frameHeight = getSize().height;
+        int frameWidth = getSize().width;
+
+        int xOffSet = 50;
+        int yOffSet = 50;
+
+        int treeHeight = Collections.max(nodeLevels.values()) + 1;
+
+        int vGap = (frameHeight - 2 * yOffSet) / (treeHeight + 1);
+        int y = vGap - yOffSet;
+
+        for (int i = 0; i < treeHeight; i++) {
+            List<Node> levelList = new ArrayList<>();
+            for (int j = 0; j < nodeList.size(); j++) {
+                Node node = nodeList.get(j);
+                if (nodeLevels.get(node) == i) {
+                    levelList.add(node);
+                }
+            }
+
+            int hGap = (frameWidth - 2 * xOffSet) / (levelList.size() + 1);
+            int x = xOffSet + hGap;
+
+            for (int j = 0; j < levelList.size(); j++) {
+                Node node = levelList.get(j);
+                node.setLocation(x, y);
+                x += hGap;
+            }
+
+            y += vGap;
+        }
+
+        container.setList(nodeList);
+    }
+
+    private int calcLevel(Node node) {
+        return calcLevel(node, 0);
+    }
+
+    private int calcLevel(Node node, int level) {
+        if (node.parent == null) {
+            return level;
+        }
+
+        return calcLevel(node.parent, ++level);
+    }
+
+    public static void main(String[] args) {
+        Main UI = new Main();
+        UI.setVisible(true);
+
+        // Refresh the gui
+        Timer refreshTimer = new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UI.repaint();
+            }
+        });
+
+        refreshTimer.setRepeats(true);
+        refreshTimer.start();
     }
 }
