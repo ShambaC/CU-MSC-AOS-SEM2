35bdb2af8ea538f4196a2ea2100689bfcded6233
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-06-30 01:57:01 +0530

    Add method to find starter node
    Co-Authored-By: Ankana Ghosh <89741182+ankanaghosh2001@users.noreply.github.com>

--- Assignment 5/java/src/ParseGraph.java
+++ Assignment 5/java/src/ParseGraph.java
@@ -1,8 +1,58 @@
+// import java.lang.classfile.components.ClassPrinter;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 public class ParseGraph {
+
     public static Node findStarter(List<Node> graph) {
-        // TODO: Remove next line and write logic here
-        return null;
+
+        Node starterNode = null;
+
+    	for(int i = 0; i < graph.size(); i++){
+            boolean flag = false;
+
+            Queue<Node> queue = new ArrayDeque<>();
+            queue.add(graph.get(i));
+
+            bfs(graph.get(i), queue);
+
+            for(int j = 0; j < graph.size(); j++){
+                if (!graph.get(j).visited){
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(!flag){
+                starterNode =  graph.get(i);
+                break;
+            }
+
+            for (int j = 0; j < graph.size(); j++){
+                Node item = graph.get(j);
+                item.visited = false;
+            }
+
+        }
+        return starterNode;
+    }
+
+
+    public static void bfs(Node i, Queue<Node> queue){
+        i.visited = true;
+
+        while(!queue.isEmpty()){
+            Node start = queue.poll();
+            int outChannelSize = start.outgoingChannels.size();
+
+            for(int j = 0; j < outChannelSize; j++){
+                Channel adjChannel = start.outgoingChannels.get(j);
+                if (!adjChannel.nodeB.visited){
+                    queue.add(adjChannel.nodeB);
+                    adjChannel.nodeB.visited = true;
+                }
+            }
+        }
     }
 }
