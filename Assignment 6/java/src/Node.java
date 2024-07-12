b109943ca409cdfc3aa0d28dfab43cbd5fcc4a85
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-07-12 13:41:12 +0530

    add todo

--- Assignment 6/java/src/Node.java
+++ Assignment 6/java/src/Node.java
@@ -29,6 +29,7 @@ public class Node extends JButton implements Runnable {
                 if (!isInCS) {
                     // TODO: Randomly decide if going into CS, if yes. Randomly decide if it depends on a node in CS, if yes then add that node to the connList
                     // TODO: while adding perform transmit, block, detect
+                    // TODO: if results in deadlock, set the deadlock boolean
                 }
             }
         }
